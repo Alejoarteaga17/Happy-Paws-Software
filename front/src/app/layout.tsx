3c6cace1e5db5be8d94976b14d03be6e5526901f
Camila Velez <cvelezg13@eafit.edu.co>
2026-09-10 12:02:49 -0500

    core: mejora visual de la vista de resumen

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="es">
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=contextual_token,pets,assignment,vaccines" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=add_circle,assignment,contextual_token,event,event_available,pending_actions,pets,task_alt,vaccines" />
       </head>
       <body>{children}</body>
     </html>
